e9ee8c68f16795276ff5e7100c1d89795e012ff0
valestinkelly <valestinkelly6@gmail.com>
2020-08-06 20:40:05 -0300

    Intento responsive
    Se empiza a medir la pantalla

--- src/Componentes/Header/index.js
+++ src/Componentes/Header/index.js
@@ -2,17 +2,36 @@ import React, { Component } from 'react'
 import Login from '../Login'
 
 export default class Header extends Component {
-    render() {
-        return (
-            <div class="bg-dark">
-            <nav class="navbar navbar-dark bg-dark container">
-                <a class="navbar-brand" href="#"><h1>JamChat</h1></a>
 
-                <div class="navbar-nav ">
-                    <Login/>
-                </div>
-            </nav>
-            </div>
-        )
+    constructor(props){
+        super(props)
+        this.state = {pantalla: window.innerWidth};
     }
+
+    render() {
+        console.log(this.state.pantalla);
+
+        if(this.state.pantalla<993){
+            return(
+                <div class="bg-dark">
+                <nav class="navbar navbar-dark bg-dark container">
+                    <a class="navbar-brand" href="#"><h1>JamChat</h1></a>
+                </nav>
+                </div>
+            );
+        }else{
+            return(
+                <div class="bg-dark">
+                <nav class="navbar navbar-dark bg-dark container">
+                    <a class="navbar-brand" href="#"><h1>JamChat</h1></a>
+                    <div class="navbar-nav ">
+                        <Login/>
+                    </div>
+                </nav>
+                </div>
+            );
+        }
+    }
+
+
 }
